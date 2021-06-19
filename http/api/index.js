83cc25a0626358b9c70bfe3784fb4d8c85c22348
DarcioCarvalho <darcio.nuno@gmail.com>
2021-06-19 02:00:23 -0300

    add api

---
+++ http/api/index.js
@@ -0,0 +1,7 @@
+const http = require('http')
+const data = require('./urls.json')
+
+http.createServer((req, res) => {
+  res.end(JSON.stringify(data))
+
+}).listen(3000, () => console.log('API is running'))
